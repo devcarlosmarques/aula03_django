5330bdab5b608deee764f7e6db63c3dd2ffcf8e7
Carlos Marques <dev.carlosmarques@gmail.com>
2023-12-17 16:50:40 -0300

    .

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.alunoView, name='aluno-lista'),
+    path('alunoID/<int:id>', views.alunoIDview, name='aluno-detalhe'),
+    path('contato/', views.contact_view, name='contato')
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Aluno
 from django.http import HttpResponse
+from django.urls import reverse_lazy, reverse
+from django.views.generic.edit import CreateView,UpdateView
+from .forms import AlunoForm
 
 def alunoView(request):
     alunos_list = Aluno.objects.all()
     return render(request,'main/alunos.html', {'alunos_list':alunos_list})
+
+def alunoIDview(request, id):
+    aluno = get_object_or_404(Aluno, pk=id)
+    print(aluno)
+    return render(request,'main/alunoID.html', {'aluno':aluno})
+
+def contact_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print('Name:', name)
+        print('Email:', email)
+        print('Message', message)
+    return render(request, 'main/contact.html')
+
+def aluno_create_view(request):
+    if request.method == 'POST':
+        form = AlunoForm(request.POST)
+        if form.is_valid():
+            aluno = form.save(commit=False)
+            aluno.user = request.user
+            aluno.save()
+            return redirect(reverse('aluno-list'))
+        else:
+            form = AlunoForm()
+
+        return render(request,'aluno_form.html'), {'form': form}
+
 
 
